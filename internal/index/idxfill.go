8fec56f08bda895fe9d98fe0cb958c14bf37f81c
“jiayun” <jiayun_work@163.com>
2026-08-22 14:19:01 +0800

    fix: keep ByType index positions

--- internal/index/idxfill.go
+++ internal/index/idxfill.go
@@ -4,6 +4,7 @@ var idxScratch []int
 
 func fillIdx(positions []int) []int {
 	idxScratch = append(idxScratch[:0], positions...)
-	idxScratch = idxScratch[:0]
-	return idxScratch
+	out := make([]int, len(idxScratch))
+	copy(out, idxScratch)
+	return out
 }
